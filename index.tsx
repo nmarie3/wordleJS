ac2323cfae2206e999d83c766f33d94a0bbeb338
Natasha <natasha.marie33@gmail.com>
2024-05-22 19:37:49 +0900

    first commit

---
+++ index.tsx
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import {checkGuess} from './wordleTS';
+import chalk from 'chalk';
+
+function playGame() {
+    const word = 'enemy';
+
+
+    if (guess === word);
+    console.log(chalk.green('You got it!'));
+}
